492ac94b92b2a643c646f7e7ad3f154e3ff36d11
Zain Kurnia <programmer-fe3@qwertysystem.net>
2025-06-04 14:01:06 +0700

    feat(vue-mfe-app): add `getGreeting` utility function to test transpilation on older browser target

--- playground/vue-mfe-app/microtsm.config.ts
+++ playground/vue-mfe-app/microtsm.config.ts
@@ -2,21 +2,26 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from '@microtsm/cli'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
+const conf = defineConfig({
   plugins: [vue()],
-  // build: {
-  //   rollupOptions: {
-  //     // Custom output options
-  //     output: {
-  //       format: 'esm',
-  //       entryFileNames: 'main.js',
-  //       chunkFileNames: 'chunks/[name]-[hash].js',
-  //     },
-  //   },
-  // },
+  build: {
+    minify: false,
+    // target: ['chrome80', 'firefox74', 'safari13.1', 'edge80'],
+    // target: ['chrome120', 'firefox119', 'safari17.4', 'edge120'],
+    rollupOptions: {
+      // Custom output options
+      output: {
+        format: 'esm',
+        entryFileNames: 'main.js',
+        chunkFileNames: 'chunks/[name]-[hash].js',
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
+
+export default conf
--- playground/vue-mfe-app/src/main.ts
+++ playground/vue-mfe-app/src/main.ts
@@ -4,6 +4,17 @@ import createVueMicroApp from '@microtsm/vue'
 import router from '@/router'
 import '@/assets/main.css'
 
+// This function uses optional chaining and nullish coalescing operator.
+// • Safari 14 (and later) supports this syntax natively.
+// • Safari 11.1 does not support these operators.
+// When targeting Safari 11.1, the Vite build step should transpile them into equivalent ES5/ES6 code.
+export function getGreeting(person) {
+  // If 'person' is null or undefined, person?.name yields undefined.
+  // Then nullish coalescing (??) provides the fallback value 'Guest'.
+  const name = person?.name ?? 'Guest'
+  return `Hello, ${name}!`
+}
+
 export const { mount, unmount } = createVueMicroApp(createApp(App), {
   el: '#app',
   handleInstance(app, props) {
