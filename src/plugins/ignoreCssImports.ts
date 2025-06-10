b3e731fea6f5aa0554cdf7c7f5345b1015a49b9e
Zain Kurnia <programmer-fe3@qwertysystem.net>
2025-06-10 12:40:34 +0700

    fix(ignoreCssImports): restrict ignored CSS imports to `style.css` only

--- src/plugins/ignoreCssImports.ts
+++ src/plugins/ignoreCssImports.ts
@@ -3,7 +3,7 @@ import { Plugin } from 'vite';
 const ignoreCssImports = (): Plugin => ({
   name: 'ignore-css-imports',
   load(id: string): string | null {
-    if (id.endsWith('.css')) return ''; // Returns an empty module, removing the CSS
+    if (id.endsWith('style.css')) return ''; // Returns an empty module, removing the CSS
     return null;
   },
 });
