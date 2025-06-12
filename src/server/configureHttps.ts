0cb8cc4a1638be66aef6732b6f636698b2809560
Zain Kurnia <programmer-fe3@qwertysystem.net>
2025-06-12 11:32:01 +0700

    fix(server): enable CORS with wildcard origin and credentials in HTTPS server configuration

--- src/server/configureHttps.ts
+++ src/server/configureHttps.ts
@@ -3,4 +3,11 @@ import mkcert from 'vite-plugin-mkcert';
 
 export default function configureHTTPSServer(conf: InlineConfig): void {
   conf.plugins = [...(conf.plugins || []), mkcert()];
+  conf.server = {
+    ...conf.server,
+    cors: {
+      origin: '*',
+      credentials: true,
+    },
+  };
 }
